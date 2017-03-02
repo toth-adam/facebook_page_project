afbb439b8af8d419e038fe0df332d164e80d4f86
adamtoth <tot.adam@gmail.com>
2017-03-02 17:42:43 +0100

    Almost workin (Problem with UTC timezone)

--- bulk_scheduler/app.js
+++ bulk_scheduler/app.js
@@ -6,7 +6,7 @@ const queryString = require("query-string");
 
 const app = express();
 
-const SCHEDULER_APP_ID = "";
+const SCHEDULER_APP_ID = "1108439522617596";
 const SCHEDULER_APP_SECRET = "";
 const SCHEDULER_APP_REDIRECT_URI = "http://localhost:3000/fb_login";
 const FB_PAGE_ID = "";
@@ -62,17 +62,23 @@ app.get("/success", function(req, res) {
 })
 
 app.get("/post_test", function(req, res) {
-   var fbScheduleReq = request.post(FB_PAGE_END_POINT + "/photos?" + queryString.stringify({
-     "caption": "7.bejegyzés, német kép",
-     "access_token": page_token,
-     "scheduled_publish_time": 1487965345,
-     "published": false
-   }))
+  var jsonData = JSON.parse(fs.readFileSync(path.join(__dirname, "../image_generator/outputs/times.json"), "utf8"));
+  var post_number = Object.keys(jsonData).length;
+  for (var i = 1; i <= post_number; i++) {
+    var caption = jsonData[i.toString()][0];
+    var scheduleDate = jsonData[i.toString()][1];
+  
+    var fbScheduleReq = request.post(FB_PAGE_END_POINT + "/photos?" + queryString.stringify({
+      "caption": caption,
+      "access_token": page_token,
+      "scheduled_publish_time": scheduleDate,
+      "published": false
+    }))
 
-  var form = fbScheduleReq.form();
-  form.append("file", fs.createReadStream(path.join(__dirname, "../image_generator/outputs/test1.png")));
-  fbScheduleReq.on("data", data => console.log(JSON.parse(data)));
-
+    var form = fbScheduleReq.form();
+    form.append("file", fs.createReadStream(path.join(__dirname, "../image_generator/outputs/" + i.toString() + ".png")));
+    fbScheduleReq.on("data", data => console.log(JSON.parse(data)));
+  };
   res.redirect("/posted");
 })
 
--- bulk_scheduler/script.js
+++ bulk_scheduler/script.js
@@ -2,6 +2,6 @@
 
 $(function () {
     $("#fblogin").click(function() {
-        window.location.replace("https://www.facebook.com/v2.8/dialog/oauth?client_id='!!!!!!CLIENT-ID!!!!'&redirect_uri=http://localhost:3000/fb_login&scope=manage_pages,publish_pages");
+        window.location.replace("https://www.facebook.com/v2.8/dialog/oauth?client_id=1108439522617596&redirect_uri=http://localhost:3000/fb_login&scope=manage_pages,publish_pages");
     });
 });
